222946ea3072bb706d187a1f82e3e9dc8f142549
Louis3701 <louis.caron16@gmail.com>
2025-09-30 11:54:50 +0200

    Introduce `Element` class in `monstres` package as a foundation for future enhancements.

--- KotlinMonster/KotlinMonster/src/main/kotlin/monstres/Element.kt
+++ KotlinMonster/KotlinMonster/src/main/kotlin/monstres/Element.kt
@@ -1,4 +1,22 @@
-package org.example.monstres
+package monstre
 
-class Element {
+class Element(
+    val id: Int,
+    val nom: String
+) {
+    val forces: MutableSet<Element> = mutableSetOf()
+    val faiblesses: MutableSet<Element> = mutableSetOf()
+    val immunises: MutableSet<Element> = mutableSetOf()
+
+    fun ajouterForce(element: Element) {
+        forces.add(element) // ajout auto sans doublon
+    }
+
+    fun ajouterFaiblesse(element: Element) {
+        faiblesses.add(element)
+    }
+
+    fun ajouterImmunite(element: Element) {
+        immunises.add(element)
+    }
 }
